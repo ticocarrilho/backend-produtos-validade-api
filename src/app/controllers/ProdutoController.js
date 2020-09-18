20b30a01d4bace6511161c5cd315b9bb9b1a0eee
Thiago Carrilho Rodriguez <ticocarrilho@gmail.com>
2020-09-18 20:23:57 -0300

    Adicionado controlador de produtos

---
+++ src/app/controllers/ProdutoController.js
@@ -0,0 +1,54 @@
+const { Produto } = require('../models');
+
+module.exports = {
+  async index(req, res) {
+    const products = await Produto.findAll();
+    return res.json(products);
+  },
+  async show(req, res) {
+    const { productId } = req.params;
+    const product = await Produto.findByPk(productId);
+    if (!product) {
+      return res.status(404).json({ message: 'Produto não encontrado.' });
+    }
+    const { nome, preco, validade } = product;
+    res.json({ nome, preco, validade });
+  },
+  async store(req, res) {
+    const { nome, preco, validade } = req.body;
+    try {
+      const product = await Produto.create({ nome, preco, validade });
+      res.json({ product });
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: 'Não foi possível adicionar o produto.' });
+    }
+  },
+  async update(req, res) {
+    const { productId } = req.params;
+    const product = await Produto.findByPk(productId);
+    if (!product) {
+      return res.status(404).json({ message: 'Produto não encontrado.' });
+    }
+    try {
+      await product.update(req.body);
+      res.json({ message: 'Produto editado com sucesso.' });
+    } catch (error) {
+      res.status(400).json({ message: 'Não foi possível editar o produto.' });
+    }
+  },
+  async delete(req, res) {
+    const { productId } = req.params;
+    const product = await Produto.findByPk(productId);
+    if (!product) {
+      return res.status(404).json({ message: 'Produto não encontrado.' });
+    }
+    try {
+      await product.destroy();
+      res.json({ message: 'Produto excluído com sucesso.' });
+    } catch (error) {
+      res.status(400).json({ message: 'Não foi possível excluir o produto.' });
+    }
+  },
+};
