197681671ecedcc168f33be671e1871882a0096a
Thiago Carrilho Rodriguez <ticocarrilho@gmail.com>
2020-09-20 00:25:09 -0300

    Trim no campo de nome, correcao validacao de data

---
+++ server/src/routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const ProdutosController = require('../app/controllers/ProdutoController');
+const {
+  requiredFieldsPost,
+  requiredFieldsPatch,
+  returnValidation,
+} = require('./validation');
+
+const routes = express.Router();
+
+routes.get('/api/produtos', ProdutosController.index);
+routes.get('/api/produtos/:productId', ProdutosController.show);
+routes.post(
+  '/api/produtos',
+  requiredFieldsPost,
+  returnValidation,
+  ProdutosController.store
+);
+routes.patch(
+  '/api/produtos/:productId',
+  requiredFieldsPatch,
+  returnValidation,
+  ProdutosController.update
+);
+routes.delete('/api/produtos/:productId', ProdutosController.delete);
+
+module.exports = routes;
